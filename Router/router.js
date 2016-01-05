b8739371d87d4cd90975dcd1827bb6f377b78b50
Jackey.Sparrow <1109952026@qq.com>
2016-01-05 22:15:32 +0800

    format

--- Router/router.js
+++ Router/router.js
@@ -3,56 +3,56 @@
  */
 
 (function (window) {
-	function Router() {
-	}
+    function Router() {
+    }
 
-	Router.prototype.setUp = function (routeMap, defaultFn) {
-		var that = this,
-			rule, fn;
+    Router.prototype.setUp = function (routeMap, defaultFn) {
+        var that = this,
+            rule, fn;
 
-		this.routeMap = [];
-		this.defaultFn = defaultFn;
+        this.routeMap = [];
+        this.defaultFn = defaultFn;
 
-		for (var rule in routeMap) {
-			if (!routeMap.hasOwnProperty(rule)) {
-				continue;
-			}
+        for (var rule in routeMap) {
+            if (!routeMap.hasOwnProperty(rule)) {
+                continue;
+            }
 
-			that.routeMap.push({
-				rule: new RegExp(rule, 'i'),
-				func: routeMap[rule]
-			});
-		}
-	};
+            that.routeMap.push({
+                rule: new RegExp(rule, 'i'),
+                func: routeMap[rule]
+            });
+        }
+    };
 
-	Router.prototype.start = function () {
-		var hash = location.hash,
-			route, matchResult;
-		for (var routeIndex in this.routeMap) {
-			route = this.routeMap[routeIndex];
-			matchResult = hash.match(route.rule);
-			if (matchResult) {
-				route.func.apply(window, matchResult.slice(1));
-				return;
-			}
-		}
-		this.defaultFn();
-	};
+    Router.prototype.start = function () {
+        var hash = location.hash,
+            route, matchResult;
+        for (var routeIndex in this.routeMap) {
+            route = this.routeMap[routeIndex];
+            matchResult = hash.match(route.rule);
+            if (matchResult) {
+                route.func.apply(window, matchResult.slice(1));
+                return;
+            }
+        }
+        this.defaultFn();
+    };
 
-	window.Router = Router;
+    window.Router = Router;
 })(window);
 
 var router = new Router();
 
 router.setUp({
-	'#/list/(.*)/(.*)': function (cate, id) {
-		console.log('list', cate, id);
-	},
-	'#/show/(.*)': function (cate, id) {
-		console.log('list', cate, id);
-	}
+    '#/list/(.*)/(.*)': function (cate, id) {
+        console.log('list', cate, id);
+    },
+    '#/show/(.*)': function (cate, id) {
+        console.log('list', cate, id);
+    }
 }, function () {
-	console.log('dafault router');
+    console.log('dafault router');
 });
 
 router.start();
