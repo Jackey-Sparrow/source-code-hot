111b9fc468113de858f5e5e9da12a95412d8d25b
Jackey.Sparrow <1109952026@qq.com>
2016-01-15 22:32:51 +0800

    add up

--- 12-Zepto-2016-01-04/type.js
+++ 12-Zepto-2016-01-04/type.js
@@ -3,53 +3,53 @@
  */
 var type = (function () {
 
-    var class2type = {},
-        toString = class2type.toString,
-        type = {};
+	var class2type = {},
+		toString = class2type.toString,
+		type = {};
 
-    function type(obj) {
-        return obj == null ? String(obj) :
-        class2type[toString.call(obj)] || "object"
-    }
+	function type(obj) {
+		return obj == null ? String(obj) :
+		class2type[toString.call(obj)] || "object"
+	}
 
-    function isFunction(value) {
-        return type(value) == "function"
-    }
+	function isFunction(value) {
+		return type(value) == "function"
+	}
 
-    function isWindow(obj) {
-        return obj != null && obj == obj.window
-    }
+	function isWindow(obj) {
+		return obj != null && obj == obj.window
+	}
 
-    function isDocument(obj) {
-        return obj != null && obj.nodeType == obj.DOCUMENT_NODE
-    }
+	function isDocument(obj) {
+		return obj != null && obj.nodeType == obj.DOCUMENT_NODE
+	}
 
-    function isObject(obj) {
-        return type(obj) == "object"
-    }
+	function isObject(obj) {
+		return type(obj) == "object"
+	}
 
-    function isPlainObject(obj) {
-        return isObject(obj) && !isWindow(obj) && Object.getPrototypeOf(obj) == Object.prototype
-    }
+	function isPlainObject(obj) {
+		return isObject(obj) && !isWindow(obj) && Object.getPrototypeOf(obj) == Object.prototype
+	}
 
-    function likeArray(obj) {
-        return typeof obj.length == 'number'
-    }
+	function likeArray(obj) {
+		return typeof obj.length == 'number'
+	}
 
-    var isArray = Array.isArray ||
-        function (object) {
-            return object instanceof Array
-        };
+	var isArray = Array.isArray ||
+		function (object) {
+			return object instanceof Array
+		};
 
-    type.isFunction = isFunction;
-    type.isWindow = isWindow;
-    type.isDocument = isDocument;
-    type.isObject = isObject;
-    type.isPlainObject = isPlainObject;
-    type.likeArray = likeArray;
-    type.isArray = isArray;
+	type.isFunction = isFunction;
+	type.isWindow = isWindow;
+	type.isDocument = isDocument;
+	type.isObject = isObject;
+	type.isPlainObject = isPlainObject;
+	type.likeArray = likeArray;
+	type.isArray = isArray;
 
-    return type;
+	return type;
 })();
 
 window.type = type;
