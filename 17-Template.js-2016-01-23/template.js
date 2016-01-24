5f671623bca3606ba9ff4b7a165256239409ad1d
Jackey.Sparrow <1109952026@qq.com>
2016-01-24 10:12:12 +0800

    template engine

---
+++ 17-Template.js-2016-01-23/template.js
@@ -0,0 +1,27 @@
+/**
+ * Created by lja on 2016-1-22.
+ */
+(function () {
+	'use strict';
+
+	var TemplateEngine = function (tpl, data) {
+		//todo:
+		var reg = /<%([^%>]+)?%>/g;
+
+		var match;// = reg.exec(tpl);
+
+		while (match = reg.exec(tpl)) {
+			tpl = tpl.replace(match[0], data[match[1]]);
+		}
+
+		return tpl;
+	};
+
+	var template = '<p>Hello, my name is <%name%>. I\'m <%age%> year old.</p>';
+
+	console.log(TemplateEngine(template, {
+		name: 'Jackey',
+		age: 25
+	}));
+
+})();
