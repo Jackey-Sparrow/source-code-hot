1c0628d643c0fa5ab3c6a5d6d26e1950369030b0
Jackey.Sparrow <1109952026@qq.com>
2016-01-27 19:13:16 +0800

    animate

--- 12-Zepto-2016-01-14/animate.js
+++ 12-Zepto-2016-01-14/animate.js
@@ -1,58 +1,59 @@
 /**
  * Created by lja on 2016-1-27.
  */
-;(function ($, Type) {
-	'use strict';
+;
+(function ($, Type) {
+    'use strict';
 
-	var prefix = '',
-		eventPrefix,
-		vendors = {Webkit: 'webkit', Moz: '', O: 'o'},
-		testEl = document.createElement('div'),
-		supportedTransforms = /^((translate|rotate|scale)(X|Y|Z|3d)?|matrix(3d)?|perspective|skew(X|Y)?)$/i,
+    var prefix = '',
+        eventPrefix,
+        vendors = {Webkit: 'webkit', Moz: '', O: 'o'},
+        testEl = document.createElement('div'),
+        supportedTransforms = /^((translate|rotate|scale)(X|Y|Z|3d)?|matrix(3d)?|perspective|skew(X|Y)?)$/i,
 
-		transform,
-		transitionProperty,
-		transitionDuration,
-		transitionTiming,
-		transitionDelay,
+        transform,
+        transitionProperty,
+        transitionDuration,
+        transitionTiming,
+        transitionDelay,
 
-		animationName,
-		animationDuration,
-		animationTiming,
-		animationDelay,
+        animationName,
+        animationDuration,
+        animationTiming,
+        animationDelay,
 
-		cssReset = {};
+        cssReset = {};
 
-	function normalizeEvent(name) {
-		return eventPrefix ? eventPrefix + name : name.toLowerCase();
-	}
+    function normalizeEvent(name) {
+        return eventPrefix ? eventPrefix + name : name.toLowerCase();
+    }
 
-	//-webkit-
-	//webkit
-	$.each(vendors, function (vendor, event) {
-		if (testEl.style[vendor + 'TransitionProperty'] !== undefined) {
-			prefix = '-' + vendor.toLowerCase() + '-';
-			eventPrefix = event;
-			return false;
-		}
-	});
+    //-webkit-
+    //webkit
+    $.each(vendors, function (vendor, event) {
+        if (testEl.style[vendor + 'TransitionProperty'] !== undefined) {
+            prefix = '-' + vendor.toLowerCase() + '-';
+            eventPrefix = event;
+            return false;
+        }
+    });
 
-	transform = prefix + 'transform';
-	cssReset[transitionProperty = prefix + 'transition-property'] =
-		cssReset[transitionDuration = prefix + 'transition-duration'] =
-			cssReset[transitionDelay = prefix + 'transition-delay'] =
-				cssReset[transitionTiming = prefix + 'transition-timing-function'] =
-					cssReset[animationName = prefix + 'animation-name'] =
-						cssReset[animationDuration = prefix + 'animation-duration'] =
-							cssReset[animationDelay = prefix + 'animation-delay'] =
-								cssReset[animationTiming = prefix + 'animation-timing-function'] = '';
+    transform = prefix + 'transform';
+    cssReset[transitionProperty = prefix + 'transition-property'] =
+        cssReset[transitionDuration = prefix + 'transition-duration'] =
+            cssReset[transitionDelay = prefix + 'transition-delay'] =
+                cssReset[transitionTiming = prefix + 'transition-timing-function'] =
+                    cssReset[animationName = prefix + 'animation-name'] =
+                        cssReset[animationDuration = prefix + 'animation-duration'] =
+                            cssReset[animationDelay = prefix + 'animation-delay'] =
+                                cssReset[animationTiming = prefix + 'animation-timing-function'] = '';
 
 
-	$.fx = {
-		off: (eventPrefix === undefined && testEl.style.transitionProperty === undefined),
-		speeds: {_default: 400, fast: 200, slow: 600},
-		cssPrefix: prefix,
-		transitionEnd: normalizeEvent('TransitionEnd'),
-		animationEnd: normalizeEvent('AnimationEnd')
-	};
+    $.fx = {
+        off: (eventPrefix === undefined && testEl.style.transitionProperty === undefined),
+        speeds: {_default: 400, fast: 200, slow: 600},
+        cssPrefix: prefix,
+        transitionEnd: normalizeEvent('TransitionEnd'),
+        animationEnd: normalizeEvent('AnimationEnd')
+    };
 })(Zepto, Type)
