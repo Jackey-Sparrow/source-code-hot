30bd18f9ea120719c13288fa9e45a0b69361c94c
Jackey.Sparrow <1109952026@qq.com>
2016-02-02 18:38:37 +0800

    format

--- 17-Template.js-2016-01-23/template.js
+++ 17-Template.js-2016-01-23/template.js
@@ -2,26 +2,26 @@
  * Created by lja on 2016-1-22.
  */
 (function () {
-	'use strict';
-	//todo: not finish
-	var TemplateEngine = function (tpl, data) {
-		//todo:
-		var reg = /<%([^%>]+)?%>/g;
+    'use strict';
+    //todo: not finish
+    var TemplateEngine = function (tpl, data) {
+        //todo:
+        var reg = /<%([^%>]+)?%>/g;
 
-		var match;// = reg.exec(tpl);
+        var match;// = reg.exec(tpl);
 
-		while (match = reg.exec(tpl)) {
-			tpl = tpl.replace(match[0], data[match[1]]);
-		}
+        while (match = reg.exec(tpl)) {
+            tpl = tpl.replace(match[0], data[match[1]]);
+        }
 
-		return tpl;
-	};
+        return tpl;
+    };
 
-	var template = '<p>Hello, my name is <%name%>. I\'m <%age%> year old.</p>';
+    var template = '<p>Hello, my name is <%name%>. I\'m <%age%> year old.</p>';
 
-	console.log(TemplateEngine(template, {
-		name: 'Jackey',
-		age: 25
-	}));
+    console.log(TemplateEngine(template, {
+        name: 'Jackey',
+        age: 25
+    }));
 
 })();
