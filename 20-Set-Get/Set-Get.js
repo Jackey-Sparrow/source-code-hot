68bed7f790c4045e4e41f8879c5a6f4978e6ba33
Jackey.Sparrow <1109952026@qq.com>
2016-02-05 20:24:22 +0800

    get set

---
+++ 20-Set-Get/Set-Get.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Jackey Li on 2016/2/5.
+ */
+(function () {
+    'use strict';
+
+    var methods = ['set', 'get'];
+
+    methods.forEach(function (method) {
+
+    });
+
+    //one param: string
+    //multi param
+    //function
+    //array
+    function create() {
+        if (arguments.length == 1) {
+            //if(){}
+        }
+    }
+})();
