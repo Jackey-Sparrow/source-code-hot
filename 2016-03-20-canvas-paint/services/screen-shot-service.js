1eab4024eaeea4e613bef093a8e995d005152f41
Jackey.Sparrow <1109952026@qq.com>
2016-03-20 11:32:26 +0800

    add service

--- 2016-03-20-canvas-paint/services/screen-shot-service.js
+++ 2016-03-20-canvas-paint/services/screen-shot-service.js
@@ -1,3 +1,18 @@
 /**
  * Created by Jackey Li on 2016/3/20.
  */
+(function (angular) {
+    'use strict';
+
+    angular.module('screenShot').factory('screenShotService',
+        [function () {
+
+            var defaultOptions = {
+                selector: 'body'
+            };
+
+            function screenShotOptions(options) {
+                angular.extend();
+            }
+        }]);
+})(angular);
