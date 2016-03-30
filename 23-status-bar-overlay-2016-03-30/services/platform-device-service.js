bf3a36b62b72838b5f3fb292250cbfd9d53daddc
Jackey.Sparrow <1109952026@qq.com>
2016-03-30 10:05:49 +0800

    refactor

--- 23-status-bar-overlay-2016-03-30/services/platform-device-service.js
+++ 23-status-bar-overlay-2016-03-30/services/platform-device-service.js
@@ -1,38 +1,23 @@
-/*
- * $Id: platform-device-service.js 321194 2015-12-21 13:37:19Z tommy $
- * Copyright (c) RIB Software AG
- */
-/* globals globalLanguages */
+(function (angular, $) {
+    'use strict';
+    angular.module('statusBarOverlay', []).factory('statusBarOverlayService', statusBarOverlayService);
 
-(function () {
-	'use strict';
-	angular.module('screenShot').factory('statusBarService', platformsDeviceService);
+    function statusBarOverlayService() {//jshint ignore:line
+        var device = {};
+        var ua = navigator.userAgent;
 
-	function platformsDeviceService() {//jshint ignore:line
-		var device = {};
-		var ua = navigator.userAgent;
-		var $ = jQuery;
+        var ipad = ua.match(/(iPad).*OS\s([\d_]+)/);
+        var ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/);
+        var iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/);
 
-		var ipad = ua.match(/(iPad).*OS\s([\d_]+)/);
-		var ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/);
-		var iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/);
+        device.webView = (iphone || ipad || ipod) && ua.match(/.*AppleWebKit(?!.*Safari)/i);
 
-		// Webview
-		device.webView = (iphone || ipad || ipod) && ua.match(/.*AppleWebKit(?!.*Safari)/i);
+        // Check for status bar and fullscreen app mode
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+        device.statusBar = false;
+        device.statusBar = device.webView && (windowWidth * windowHeight === screen.width * screen.height) ? true : false;
 
-		// Check for status bar and fullscreen app mode
-		var windowWidth = $(window).width();
-		var windowHeight = $(window).height();
-		device.statusBar = false;
-		if (device.webView && (windowWidth * windowHeight === screen.width * screen.height)) {
-			device.statusBar = true;
-		}
-		else {
-			device.statusBar = false;
-		}
-
-
-		// Export object
-		return device;
-	}
-})();
+        return device;
+    }
+})(angular, $);
