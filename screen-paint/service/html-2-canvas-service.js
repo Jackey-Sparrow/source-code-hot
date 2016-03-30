4e79f8430708accfba359678eb0586000287b1e1
Jackey.Sparrow <1109952026@qq.com>
2016-03-30 09:55:46 +0800

    add service

---
+++ screen-paint/service/html-2-canvas-service.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Jackey Li on 2016/3/30.
+ */
+(function (angular) {
+    'use strict';
+
+    angular.module('html2canvasService',
+        [function () {
+                return {
+
+                };
+        }]);
+})(angular);
