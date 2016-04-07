e40d4bc6a07642ceddeffb2090e932784d0fd76c
Jackey.Sparrow <1109952026@qq.com>
2016-04-07 20:13:19 +0800

    add fn

--- 25-cordova-back-button-2016-04-07/cordova-back-button.js
+++ 25-cordova-back-button-2016-04-07/cordova-back-button.js
@@ -28,6 +28,18 @@
 
 					return {
 
+						//var Backbutton = {
+                        //
+						//	goHome: function(successCallback, failureCallback) {
+						//		cordova.exec(successCallback, failureCallback, 'BackbuttonPlugin',
+						//			'goHome', []);
+						//	},
+						//	goBack: function(successCallback, failureCallback) {
+						//		cordova.exec(successCallback, failureCallback, 'BackbuttonPlugin',
+						//			'goBack', []);
+						//	}
+						//};
+
 						clearBackButtonWatch: function () {
 							document.removeEventListener('backbutton', backButtonEvent);
 							$rootScope.$$listeners['$cordovaNetwork'] = [];
