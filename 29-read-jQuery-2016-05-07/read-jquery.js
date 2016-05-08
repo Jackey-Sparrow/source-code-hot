aee508ca7c8a432d57bf16e029d4e967c46bfc6c
Jackey.Sparrow <1109952026@qq.com>
2016-05-08 19:26:25 +0800

    add module check

--- 29-read-jQuery-2016-05-07/read-jquery.js
+++ 29-read-jQuery-2016-05-07/read-jquery.js
@@ -1,3 +1,22 @@
 /**
  * Created by Jackey Li on 2016/5/7.
  */
+(function (global, factory) {
+
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        //var jQuery = require("jquery")(window);
+        module.exports = glbal.document ? factory(global, true) : function (w) {
+            if (!w.document) {
+                throw new Error('');
+            }
+            return factory(w);
+        };
+    } else {
+        factory(global);
+    }
+
+
+})(typeof window !== 'undefined' ? window : this, function (window, noGlobal) {
+
+    }
+);
