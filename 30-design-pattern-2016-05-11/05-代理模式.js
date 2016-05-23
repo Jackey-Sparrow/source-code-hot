44b2065742ccccde9a56d74898bf2e0926320bf7
Jackey <1109952026@qq.com>
2016-05-23 10:25:03 +0800

    proxy

--- 30-design-pattern-2016-05-11/05-代理模式.js
+++ 30-design-pattern-2016-05-11/05-代理模式.js
@@ -1,3 +1,23 @@
 /**
  * Created by mobilitymacbook on 5/12/16.
  */
+var Girl = function(name){
+	this.name = name;
+};
+
+var Dudu = function(girl){
+	this.girl = girl;
+	this.sendGift = function(gift){
+		console.log('hi'+this.girl.name+', dudu send you '+ gift);
+	};
+};
+
+var Proxy = function (girl) {
+	this.girl = girl;
+	this.sendGift= function (gift) {
+		(new Dudu(girl)).sendGift(gift);
+	};
+};
+
+var proxy = new Proxy(new Girl('Cassi'));
+proxy.sendGift('flowers');
