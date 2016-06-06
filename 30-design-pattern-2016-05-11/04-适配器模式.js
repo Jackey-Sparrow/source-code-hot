8fbb6c07227ddb726031adb178cb5fd93db0ca1b
Jackey <1109952026@qq.com>
2016-06-06 10:17:28 +0800

    adapter

--- 30-design-pattern-2016-05-11/04-适配器模式.js
+++ 30-design-pattern-2016-05-11/04-适配器模式.js
@@ -1,3 +1,18 @@
 /**
  * Created by mobilitymacbook on 5/12/16.
  */
+
+
+var obj = {
+  str1:'',
+  str2:'',
+  str3:''
+};
+
+var fn = function(str1,str2,str3){
+  //...
+};
+
+var adapter = function(obj){
+  fn(obj.str1,obj.str2,obj.str3);
+};
